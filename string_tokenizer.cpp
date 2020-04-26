1a87b7cbbc5ebe2a667680e0a6e80a45949c4732
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-04-26 19:17:30 +0530

    Create string_tokenizer.cpp

---
+++ string_tokenizer.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<cstring>
+#include<string>
+using namespace std;
+
+char *mystrtok(char *str,char delim)
+{
+   static char *input=NULL;
+   if( str!=NULL)
+   {
+       input=str;
+   }
+   if(input==NULL)
+   {
+       return NULL;
+   }
+   char *output=new char[strlen(input)+1];
+   int i=0;
+   for(;input[i]!='\0';i++)
+   {
+       if(input[i]!=delim)
+       {
+           output[i]=input[i];
+       }
+       else{
+           output[i]='\0';
+           input=input+i+1;
+           return output;
+       }
+   }
+   output[i]='\0';
+   input=NULL;
+   return output;
+}
+int main()
+{
+	char s[100]="Hi my name is Akshita";
+    char *ptr=mystrtok(s,' ');
+    cout<<ptr<<endl;
+    while(ptr!=NULL)
+        
+        {
+            ptr=mystrtok(NULL,' ');
+            cout<<ptr<<endl;
+        }
+	return 0;
+}
