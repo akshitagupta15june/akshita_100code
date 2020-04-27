d58d8caa9a88779b3439b636c1f58c69ceeb1c2e
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-04-27 21:23:13 +0530

    Create quiz.cpp

---
+++ quiz.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long signed CodingBlocks = 2017;
+    short unsigned BOSS1     = -2018;
+    unsigned BOSS2           = -2019;
+    int      BOSS3           = -2020;
+    long long unsigned BOSS4 = -2021;
+    short unsigned Nagarro   = 2018.9;
+    long signed HackerBlocks = 'A';
+    cout<<CodingBlocks<<endl;
+    cout<<BOSS1<<endl<<BOSS2<<endl<<BOSS3<<endl<<BOSS4<<endl;
+    cout<<Nagarro<<endl;
+    cout<<HackerBlocks<<endl;
+
+
+    return 0;
+    }
