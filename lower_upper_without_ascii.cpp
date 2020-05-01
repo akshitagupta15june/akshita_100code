abe733a6f59aa5db24d6035393217d7d7e2fa565
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-05-01 18:00:34 +0530

    Update lower_upper_without_ascii.cpp

--- lower_upper_without_ascii.cpp
+++ lower_upper_without_ascii.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<cctype>
+#include<cstring>
+#include<cstdio>
 
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if (isupper(ch))
+    {
+        cout<<"U";
+    }
+    else if (islower(ch))
+    {
+        cout<<"L";
+    }
+    else{
+        cout<<"I";
+    }
+    
+    return 0;
+}
