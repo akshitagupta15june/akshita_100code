0467a2d6aa501e5d4cd9873ee8a6ed5c3ba1a132
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-05-01 15:50:15 +0530

    Create lower_upper.cpp

---
+++ lower_upper.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cctype>
+#include<cstring>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(ch>=65&&ch<=90)
+    {
+        cout<<"UPPERCASE";
+    }
+		
+	
+    else if(ch>=97&&ch<=122)
+    {
+        cout<<"lowercase";
+    }
+        
+    else{
+        cout<<"Invalid";
+    }
+    return 0;
+}
