affdf7b6cc30168ee4807ef8ca197759c2dbc23c
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-05-04 15:42:11 +0530

    Create 2d-dma.cpp

---
+++ 2d-dma.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int **arr;
+    int r,c;
+    cin>>r>>c;
+    arr=new int*[r];
+    for(int i=0;i<r;i++)
+    {
+        arr[i]=new int[c];
+    }
+    int val=1;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            arr[i][j]=val;
+            val++;
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
