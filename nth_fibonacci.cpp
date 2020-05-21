87d0fc785a6ed4a5ef785183efe8d49444352e24
AKSHITA GUPTA <57909583+akshitagupta15june@users.noreply.github.com>
2020-05-21 13:42:52 +0530

    Create nth_fibonacci.cpp

---
+++ nth_fibonacci.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+    if(n==0 or n==1)
+    {
+        return n;
+    }
+    return fib(n-1)+fib(n-2);
+}
+int main(int argc, char **argv)
+{
+	int n;
+    cin>>n;
+    cout<<fib(n)<<endl;
+	return 0;
+}
